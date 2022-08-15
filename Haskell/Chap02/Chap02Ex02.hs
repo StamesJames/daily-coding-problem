9f9cd22dba11f8b8c19da14b3ae42b0c3b79a2d1
Stames <benedict.smit@udo.edu>
2022-08-15 08:53:30 +0200

    alternative Haskell Solution for Chap02Ex02 with a quadratic time in the longest word, not the count of words

--- Haskell/Chap02/Chap02Ex02.hs
+++ Haskell/Chap02/Chap02Ex02.hs
@@ -1,11 +1,13 @@
 module Haskell.Chap02.Chap02Ex02 where
 import Haskell.Utils.Utils (enumerate)
 import Control.Monad (guard)
+import qualified Data.Map as Map
 
 
--- Exercise: Find all pairs of indices so the concatinated strings form a Palindrom
 
--- Solution: Because we build all pairs of elementes and check for every concatinatet string if it's a palindrom we our algorithm is in O(n^2 * w) where n is the count of strings and w is the length of the longest string. 
+-- Exercise: Find all pairs of indices so the concatenated strings form a Palindrom
+
+-- Solution: Because we build all pairs of elements and check for every concatenated string if it's a palindrome we our algorithm is in O(n^2 * w) where n is the count of strings and w is the length of the longest string. 
 
 example = ["code", "edoc", "da", "d"]
 
@@ -23,4 +25,19 @@ findPalindromPairsMonadStyle ws = do
     guard $ isPalindrom (x ++ y)
     return (i,j)
 
+-- Solution: another Solution is to look for every word if its suffix is a palindrome and then if the reversed prefix is in our list. If we would use a constant lookup Map we would get a an algorithm in O(n * w^2)
 
+putInMap ws = foldl (\m (i, elem) -> Map.insert elem i m) Map.empty (enumerate ws)
+
+allSuffixes [] = [""]
+allSuffixes w@(x:xs) = w : allSuffixes xs 
+
+-- I've written this function in this way just for practice. for a faster solution we could get rid of some reverses, because in the function findPalindromPairsQuadratInWord we use the words reversed anyways 
+allPrefixes = reverse . map reverse . allSuffixes . reverse
+
+findPalindromPairsQuadratInWord ws = do
+    let m = putInMap ws
+    w <- ws
+    (prefRev, suf) <- zip (map reverse $ allPrefixes w) (allSuffixes w)
+    guard $ isPalindrom suf && Map.member prefRev m
+    return (m Map.! w, m Map.! prefRev)
